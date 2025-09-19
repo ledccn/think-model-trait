be992d1567c7e4b3f30ea43848428366f2e35e38
david <367013672@qq.com>
2025-09-19 23:02:33 +0800

    新增：geoRadius 查询地理位置范围内符合条件的成员

--- src/Traits/HasRedisGeo.php
+++ src/Traits/HasRedisGeo.php
@@ -2,12 +2,14 @@
 
 namespace Ledc\ThinkModelTrait\Traits;
 
+use BadMethodCallException;
 use Ledc\ThinkModelTrait\RedisUtils;
 use Redis;
 use RedisException;
 
 /**
  * Redis GEO地理位置
+ * @method array geoRadiusByMember($member, $radius, $units, $options = []) 根据储存在位置集合里面的某个地点获取指定范围内的地理位置集合
  */
 trait HasRedisGeo
 {
@@ -100,6 +102,21 @@ trait HasRedisGeo
     }
 
     /**
+     * 查询地理位置范围内符合条件的成员
+     * - 该方法用于检索某个地理位置坐标范围内指定半径内的所有成员，并支持多种选项来控制返回结果的格式和内容。
+     * @param float $lng 查询中心点的经度
+     * @param float $lat 查询中心点的纬度
+     * @param float $radius 搜索半径
+     * @param string $unit 距离单位（如 'm'、'km'、'mi'、'ft'）
+     * @param array $options 可选参数数组，控制返回结果格式
+     * @return mixed
+     */
+    public function geoRadius(float $lng, float $lat, float $radius, string $unit = Constants::UNIT_M, array $options = [])
+    {
+        return static::connection()->georadius($this->getGeoKey(), $lng, $lat, $radius, $unit, $options);
+    }
+
+    /**
      * 以各种方式搜索地理空间集合中的成员
      * @param array|string $position 一个包含经纬度的数组，或一个集合成员的字符串【示例 [$longitude, $latitude] 或 $member】
      * @param array|int|float $shape 一个数字表示搜索的圆的半径，或者一个双元素数组表示要搜索的框的宽度和高度
@@ -125,6 +142,21 @@ trait HasRedisGeo
     public function geoSearchStore(string $dst, string $src, $position, $shape, string $unit = Constants::UNIT_M, array $options = [])
     {
         return static::connection()->geosearchstore($dst, $src, $position, $shape, $unit, $options);
+    }
+
+
+    /**
+     * 动态调用
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     */
+    public function __call(string $name, array $arguments)
+    {
+        if (0 !== strpos(strtolower($name), 'geo')) {
+            throw new BadMethodCallException('Call to undefined method ' . self::class . '::' . $name . '()');
+        }
+        return $this->connection()->{$name}($this->getGeoKey(), ...$arguments);
     }
 
     /**
